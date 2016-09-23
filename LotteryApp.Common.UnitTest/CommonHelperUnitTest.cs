65223c6fa4682a4da0a5cf4487b901f399518bb6
Mukesh Rawat <mukeshrawat02@gmail.com>
2016-09-23 16:26:06 +0100

    Simple unit-test on LotteryNumber Generator. TODO-- Need to add more scenarios.

---
+++ LotteryApp.Common.UnitTest/CommonHelperUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LotteryApp.Common.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LotteryApp.Common.UnitTest
+{
+    [TestClass]
+    public class CommonHelperUnitTest
+    {
+        [TestMethod]
+        public void GenerateLotteryNumbersTest()
+        {
+            var items = LotteryHelper.GenerateLotteryNumbers(6, 1, 49);
+            Assert.AreEqual<int>(6, items.Count);
+        }
+    }
+}
