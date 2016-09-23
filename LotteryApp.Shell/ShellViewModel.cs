ba0d10564222c22d6af63cae0e2ef388bbd32f13
Mukesh Rawat <mukeshrawat02@gmail.com>
2016-09-23 15:37:48 +0100

    Added MVVM (Prism.Mvvm) dependencies. Also added Shell and Default themes and changed the default Window to MetroWindow.

---
+++ LotteryApp.Shell/ShellViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LotteryApp.Common.Utils;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace LotteryApp.Shell
+{
+    public class ShellViewModel : BindableBase
+    {
+
+        public DelegateCommand<object> GenerateLotteryNumberCommand { get; set; }
+
+        public ShellViewModel()
+        {
+            GenerateLotteryNumberCommand = new DelegateCommand<object>(OnNumberChange);
+        }
+        private IEnumerable<int> _lotteryNumbers;
+        public IEnumerable<int> LotteryNumbers
+        {
+            get { return _lotteryNumbers; }
+            set
+            {
+                _lotteryNumbers = value;
+                OnPropertyChanged(() => this.LotteryNumbers);
+            }
+        }
+
+        public void OnNumberChange(object param)
+        {
+            LotteryNumbers = LotteryHelper.GenerateLotteryNumbers(6, 1, 49);
+        }
+    }
+}
