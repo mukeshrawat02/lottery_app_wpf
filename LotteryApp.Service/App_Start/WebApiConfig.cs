55c850618db982ed059d7cbac0346a32bbe41a23
Mukesh Rawat <mukeshrawat02@gmail.com>
2016-09-23 16:23:43 +0100

    Added API Controller which gets LotteryNumbers from Common.Helper (LotteryHelper).

---
+++ LotteryApp.Service/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace LotteryApp.Service
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "GetLotteryNumber",
+                routeTemplate: "api/{controller}/{action}/{sequenceCount}/{startNumber}/{endNumber}"
+             );
+        }
+    }
+}
---
+++ LotteryApp.Service/Controllers/LotteryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LotteryApp.Common.Utils;
+
+namespace LotteryApp.Service.Controllers
+{
+    public class LotteryController : ApiController
+    {
+        public IEnumerable<int> GetLotteryNumbers(int count, int startNumber, int endNumber)
+        {
+            return LotteryHelper.GenerateLotteryNumbers(count, startNumber, endNumber);
+        }
+    }
+}
